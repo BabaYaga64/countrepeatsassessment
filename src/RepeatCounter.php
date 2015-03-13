bddef00dbec30c2570247132f4a86a3834f2e7b2
Bojana Skarich <bojana.skarich@gmail.com>
2015-03-13 10:41:00 -0700

    Add first test to test file, testing a 1 letter word in a one letter string. Add PHPUnit files. Create composer.json file.

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -5,6 +5,7 @@
 
 		function CountRepeats($input_word, $string)
 		{
+			
 
 		}//Ends CountRepeats
 
--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -1,5 +1,40 @@
 <?php
 
-	require_once "src/RepeatCounter.php"
+	require_once "src/RepeatCounter.php";
+
+	class RepeatCounterTest extends PHPUnit_Framework_TestCase
+	{
+
+		function test_1letter_1char {
+
+			//Arrange
+			$test_1letter_1char = new RepeatCounter;
+			$input1 = "a";
+			$input2 = "a";
+
+			//Act
+			$result = $test_RepeatCounter->CountRepeats($input1, $input2);
+
+			//Assert
+			$this->assertEquals(1, $result);
+
+		}//Ends test_1letter_1char
+
+		// function test_1letter_1char {
+
+		// 	//Arrange
+		// 	$test_1letter_1char = new RepeatCounter;
+		// 	$input1 = "a";
+		// 	$input2 = "a";
+
+		// 	//Act
+		// 	$result = $test_RepeatCounter->CountRepeats($input1, $input2);
+
+		// 	//Assert
+		// 	$this->assertEquals(1, $result);
+
+		// }//Ends test_1letter_1char
+
+	}//Ends class
 
 ?>
