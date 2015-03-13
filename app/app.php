5d26daacbebb7ad8ad497167861cbddd9bc9bdc1
Bojana Skarich <bojana.skarich@gmail.com>
2015-03-13 15:06:28 -0700

    Add content to twig files and add twig functionality to app.php routes.

--- app/app.php
+++ app/app.php
@@ -5,16 +5,27 @@
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views'
-));
+    ));
 
     $app['debug'] = true;
 
 
     $app->get("/", function() use ($app) {
 
+        });
 
+        return $app['twig']->render('inputform.twig');
+
+
+
+    $app->post("/inputform", function()  use ($app) {
+        $word = new RepeatCounter($_POST['input_word']);
+        $task->save();
+
+        return $app['twig']->render('inputform.twig');
 
     });
 
-    return $app['twig']->render('inputform.twig');
+
+
 ?>
