8dd300e70bfcb2a1e964ce555220d1d355cc77ba
Bojana Skarich <bojana.skarich@gmail.com>
2015-03-17 21:04:53 -0700

    Change variable names to be more meaninful (entire sentence).

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -3,24 +3,24 @@
 	class RepeatCounter
 	{
 
-		function CountRepeats($input_word, $input_string)
+		function CountRepeats($word_to_search_for, $string_to_search_in)
 		{
 			//Set counter to 0
 			$frequency = 0;
 
 			//Create array of strings separated by a space from the input string the user enters
-			$array_of_words = explode(" ", $input_string);
+			$array_of_words = explode(" ", $string_to_search_in);
 
 
 
 			//Run through each of the words in the array, and if the string matches the input word, then add 1. If there is no input, then return 0.
 			foreach ($array_of_words as $word) {
-				if ($word == $input_word) {
+				if ($word == $word_to_search_for) {
 					$frequency += 1;
 
 				}
 
-				elseif ($input_word == null)
+				elseif ($word_to_search_for == null)
 					return 0;
 			}
 				return $frequency;
